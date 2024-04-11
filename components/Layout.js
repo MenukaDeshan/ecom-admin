45d6ac5a6a10f64ed7571613780024a62c3c89db
Menuka Deshan <64095498+MenukaDeshan@users.noreply.github.com>
2024-04-11 14:32:24 +0530

    full

---
+++ components/Layout.js
@@ -0,0 +1,43 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "@/components/Nav";
+import {useState} from "react";
+import Logo from "@/components/Logo";
+
+export default function Layout({children}) {
+  const [showNav,setShowNav] = useState(false);
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="flex items-center w-screen h-screen bg-gray-900">
+        <div className="w-full text-center">
+          <button onClick={() => signIn('google')} className="p-2 px-4 bg-white rounded-lg">
+          <svg className="flex w-6 h-6" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+          <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+          </svg>
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-900 ">
+      <div className="items-center block p-4 md:hidden">
+        <button onClick={() => setShowNav(true)}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+          </svg>
+        </button>
+        <div className="flex justify-center mr-6 grow">
+          <Logo />
+        </div>
+      </div>
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="flex-grow p-4 mt-2 mr-2 bg-white rounded-lg h-dvh">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
